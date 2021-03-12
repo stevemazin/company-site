182f05fa919e8b9b78ef805b0f82d0b5f31338d8
stevemazin <munenekinyua.mk@gmail.com>
2021-03-12 09:36:55 +0300

    Added a form to the page

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -9,24 +9,61 @@ export default function Form() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.contactCard}>
-        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-          <input
-            className={styles.input}
-            name="firstName"
-            ref={register({ required: true, maxLength: 20 })}
-          />
-          <select name="gender" ref={register}>
-            <option value="property-managament">Property Management</option>
-            <option value="debt-collection-services">Debt Collection</option>
-            <option value="other">Other</option>
-          </select>
-          <input
-            className={styles.submitBtn}
-            value="Send Message"
-            type="submit"
-          />
-        </form>
+      <div className={styles.contactWrapper}>
+        <div className={styles.contactHeader}>
+          <h1 className={styles.contactHeaderMain}>Send Us a Message</h1>
+          <p className={styles.contactHeaderTag}>
+            We'll get back to you as soon as we can!
+          </p>
+        </div>
+        <div className={styles.formWrapper}>
+          <form id="contact-form" onSubmit={handleSubmit(onSubmit)}>
+            <div className={styles.formGroup}>
+              <label className={styles.label}>Full Name</label>
+              <input
+                className={styles.input}
+                name="fullname"
+                type="text"
+                placeholder="John Doe"
+                ref={register({ required: true, maxLength: 20 })}
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label className={styles.label}>Contact</label>
+              <input
+                className={styles.input}
+                name="contact"
+                type="text"
+                placeholder="+254700555888"
+                ref={register({ required: true, maxLength: 20 })}
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label className={styles.label}>Message</label>
+              <textarea
+                className={styles.textarea}
+                name="message"
+                type="text"
+                rows="5"
+                placeholder="Short message here..."
+                ref={register({ required: true, maxLength: 20 })}
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label className={styles.label}>Services</label>
+
+              <select className={styles.select} name="services" ref={register}>
+                <option className={styles.option} value="property-management">
+                  Property Management
+                </option>
+                <option className={styles.option} value="debt-collection">
+                  Debt Collection
+                </option>
+              </select>
+            </div>
+            <input className={styles.submitBtn} type="submit" />
+          </form>
+        </div>
       </div>
     </div>
   );
